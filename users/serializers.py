45f1f9ae61f68c36b516a85f7ee539b744bce2ba
Alaa <alahamadneh35@gmail.com>
2025-04-21 12:21:55 +0300

    updated UserRolesSerializer to return role_id and role_name

--- users/serializers.py
+++ users/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Users
+from .models import Users,UserRoles
 from django.contrib.auth.hashers import make_password
 
+class UserRolesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserRoles
+        fields = ['role_id', 'role_name']
+
 class UserSerializer(serializers.ModelSerializer):
+    role = UserRolesSerializer(read_only = True)
     class Meta:
         model = Users
         fields = ['user_id', 'username', 'email', 'password_hash', 'role', 'profile_picture', 'bio', 'created_at', 'updated_at']
